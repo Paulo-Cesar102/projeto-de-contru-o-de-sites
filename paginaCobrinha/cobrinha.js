86f56346d0a32bd7a02e95432c78b9d2cfb8432a
Paulo-Cesar102 <153522516+Paulo-Cesar102@users.noreply.github.com>
2025-05-09 16:27:05 -0300

    atu

--- paginaCobrinha/cobrinha.js
+++ paginaCobrinha/cobrinha.js
@@ -1,9 +1,9 @@
 //area do canvas
 const canvas=document.querySelector('.canvas');
-const contexto = canvas.getContext('2d');
+const cxt= canvas.getContext('2d');
 //cobrinha
 const size=30;
-const cobrinha = 
+const cobra = 
 [   {x:200, y:200},
     {x:230, y:200},             
 ];
@@ -11,51 +11,52 @@ const cobrinha =
 let direcao= ""; //direção inicial da cobrinha
 
 //função para desenhar a cobrinha
-const desenhoCobrinha = () =>{
-contexto.fillStyle='#98fb98'; //cor do corpo da cobrinha
+const desenhocobra = () =>{
+ctx.fillStyle='#98fb98'; //cor do corpo da cobrinha
 
-cobrinha.forEach((position, index) =>{
+cobra.forEach((position, index) =>{
 
-if(index === cobrinha.length -1 ){
-    contexto.fillStyle='green'; //cor da cabeça da cobrinha
+if(index === cobra.length -1 ){
+    ctx.fillStyle='green'; //cor da cabeça da cobrinha
 }
 
-contexto.fillRect(position.x, position.y, size , size);
+ctx.fillRect(position.x, position.y, size , size);
 });
 };
 
 const moveCobra = () =>{
     if(!direcao) return; //se não tiver direção, não faz nada
-    const cabeca =cobrinha[cobrinha.length -1]; //pega a última posição da cobrinha
+    const cabeca =cobra[cobra.length -1]; //pega a última posição da cobrinha
 
    if(direcao == "right"){
-cobrinha.push({x: cabeca.x + size , y:cabeca.y});
+cobra.push({x: cabeca.x + size , y:cabeca.y});
    }
 
    if(direcao == "left"){
-    cobrinha.push({x: cabeca.x - size , y:cabeca.y});
+    cobra.push({x: cabeca.x - size , y:cabeca.y});
        }
 
        if(direcao == "down"){
-        cobrinha.push({x: cabeca.x , size , y:cabeca.y + size});
+        cobra.push({x: cabeca.x , size , y:cabeca.y + size});
            }
 
            if(direcao == "up"){
-            cobrinha.push({x: cabeca.x , size , y:cabeca.y - size});
+            cobra.push({x: cabeca.x , size , y:cabeca.y - size});
                }
     
 
-   cobrinha.shift(); //remove a primeira posição da cobrinha
+   cobra.shift(); //remove a primeira posição da cobrinha
 }
-
+ 
 const gameloop= () =>{
 
 }
 
 setInterval(() => {
-    contexto.clearRect(0,0, 600, 600); //limpa o canvas
+    ctx.clearRect(0,0, 600, 600); //limpa o canvas
     moveCobra()
-    desenhoCobrinha()
+    desenhocobra()
 },300) //atualiza a cada 300ms
+
 
 
