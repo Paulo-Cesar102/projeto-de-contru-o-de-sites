0603c8a1b9396e0310e03531997b9641f5698f82
Paulo-Cesar102 <153522516+Paulo-Cesar102@users.noreply.github.com>
2025-06-01 00:10:56 -0300

    f

---
+++ JogoDaCalculadora/calculadora.js
@@ -0,0 +1,40 @@
+let insert = (num) => {
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML += num;
+};
+
+let apagar = () => {
+    document.getElementById('resultado').innerHTML = '';
+};
+
+let voltar = () => {
+    let numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero.slice(0, -1);
+};
+
+let calcular = () => {
+    let numero = document.getElementById('resultado').innerHTML;
+    const resultado = document.getElementById('resultado');
+    const explosaoImg = document.getElementById('explosao');
+    const som = document.getElementById('somExplosao');
+
+    if (numero.includes('/0')) {
+        resultado.innerHTML = '💥 BOOM!';
+        resultado.classList.add('explodiu');
+
+        // Mostra imagem
+        explosaoImg.style.display = 'block';
+
+        // Toca o som
+        som.play();
+
+        // Depois de 1 segundo, limpa tudo
+        setTimeout(() => {
+            resultado.innerHTML = '';
+            resultado.classList.remove('explodiu');
+            explosaoImg.style.display = 'none';
+        }, 1500);
+    } else if (numero) {
+        resultado.innerHTML = eval(numero);
+    }
+};
